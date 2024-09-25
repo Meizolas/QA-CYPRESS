37e22214a1b28651ef3788efb4fde1b464b04407
Hélio <HelioJR780@gmail.com>
2024-09-25 10:12:15 -0300

    QA-CYPRESS

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin');
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123');
+    cy.get('.oxd-button').click();
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard');
+  })
+})
